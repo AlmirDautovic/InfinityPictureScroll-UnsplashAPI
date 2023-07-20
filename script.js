fa3ccce7206f059aa77b144bcaa960c6ea2f1749
AlmirDautovic <aalmir.d@gmail.com>
2023-07-20 23:26:51 +0200

    made new function that will create elements and input fetched data and create content on page.added another function that will set atributes on each element

--- script.js
+++ script.js
@@ -8,11 +8,35 @@ let photosArray = [];
 
 const unsplashApiUrl = `https://api.unsplash.com/photos/random/?client_id=${accesKey}&count=${count}`;
 
+setAttributes = (element, attributes) => {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+createElementsToDisplayPhotos = () => {
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: "_blank"
+        });
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        item.appendChild(img);
+        imgWrapper.appendChild(item);
+    });
+}
+
 getPhotos = async () => {
     try {
         const response = await fetch(unsplashApiUrl);
         photosArray = await response.json();
-        console.log(photosArray);
+        createElementsToDisplayPhotos();
     }
     catch (err) {
         console.log(err);
