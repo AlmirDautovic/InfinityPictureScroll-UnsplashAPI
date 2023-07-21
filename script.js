771c3fc9b11be9cf761d78bac87b316a13279ed7
AlmirDautovic <aalmir.d@gmail.com>
2023-07-21 23:37:35 +0200

    started to make functionallity where more picture will be loaded after scrolling to certain page height

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const count = 10;
+const count = 30;
 const accesKey = 'r7ENJFDxlBj7pkGMZSky40GH3nnuhBcPcWu6fWlQPYU';
 
 const imgWrapper = document.getElementById('wrapper');
@@ -7,6 +7,10 @@ const loader = document.getElementById('loading-cube');
 let photosArray = [];
 
 const unsplashApiUrl = `https://api.unsplash.com/photos/random/?client_id=${accesKey}&count=${count}`;
+
+imageLoaded = () => {
+    console.log('image loaded');
+}
 
 setAttributes = (element, attributes) => {
     for (const key in attributes) {
@@ -27,6 +31,9 @@ createElementsToDisplayPhotos = () => {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+
+        img.addEventListener('load', imageLoaded);
+
         item.appendChild(img);
         imgWrapper.appendChild(item);
     });
@@ -42,5 +49,11 @@ getPhotos = async () => {
         console.log(err);
     }
 }
+
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000) {
+        getPhotos();
+    }
+})
 
 getPhotos();
